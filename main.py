3d22b9f630655ff45d26249f8e81ee80d7b0a855
Iván <ilv0003@alu.medac.es>
2024-02-14 23:21:40 +0100

    Commit

--- main.py
+++ main.py
@@ -23,7 +23,21 @@ def read_item(item_id: int, q: Union[str, None] = None):
 # PARAMETRO ================> RESPUESTA
 # Ejemplo "hola" => {"cadena": "hola", "longitud": 4, "vocales": 2}
 # Ejemplo "estamosenclasededam" => {"cadena": "estamosenclasededam", "longitud": 19, "vocales": 7}
-@app.get("/calcular-longitud/{cadena}")
-def read_item(cadena: str):
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/calcular-longitud/")
+def calcular_longitud(cadena: str = "Esta cadena tiene una longitud pequeña"):
     num_vocales = sum(1 for char in cadena if char.lower() in "aeiou")
     return {"cadena": cadena, "longitud": len(cadena), "vocales": num_vocales}
